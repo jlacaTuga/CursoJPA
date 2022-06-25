179de9d85518f427b2d5cbbd258fad5777b3abcc
José Luís <jlaca@live.com.pt>
2022-06-25 14:46:31 -0300

    Rest esta funcionando

---
+++ src/test/java/com/tuga/cursojpa/CursoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuga.cursojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
