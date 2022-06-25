c1c83452a8b57bed279f6d28a85098304c1d8198
José Luís <jlaca@live.com.pt>
2022-06-25 15:08:31 -0300

    Testando a primeira classe de dominio: Categorias

--- src/main/java/com/tuga/cursojpa/resources/CategoriaResource.java
+++ src/main/java/com/tuga/cursojpa/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package com.tuga.cursojpa.resources;
 
+import com.tuga.cursojpa.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Olá Mundo";
+    public List<Categoria>listar(){
+        Categoria cat1 = new Categoria(1,"Informatica");
+        Categoria cat2 = new Categoria(2,"Escritorio");
+        List<Categoria>list=new ArrayList<>();
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
     }
 
 
